8fad9f329906dcec6f2dfdd80109e45a7faabb19
Adil Alam <alamadil540@gmail.com>
2021-03-04 12:30:13 +0500

    nav finished

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img src="" alt="background image"/>
+
+            </div>
+        </div>
+    )
+};
+
+export default Home;
